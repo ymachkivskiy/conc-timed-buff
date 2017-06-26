5e98df3a476c6df9298cbd99f644aad4b4ee5944
Yaroslav Machkivskiy <ihoo.ir@gmail.com>
2017-06-26 02:05:21 +0200

    Improve NaiveMessageStorage memory usage and message exceeded keep alive time dropping

--- src/main/java/org/example/ym/concbuff/NaiveMessageStorage.java
+++ src/main/java/org/example/ym/concbuff/NaiveMessageStorage.java
@@ -2,21 +2,25 @@ package org.example.ym.concbuff;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.collect.Iterables.getLast;
+import static java.util.Arrays.asList;
+import static java.util.Collections.binarySearch;
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.comparingLong;
 import static java.util.stream.Collectors.toList;
 
 public class NaiveMessageStorage implements MessageStorage {
 
-    private final Duration keepAliveDuration;
+    private static final int INITIAL_BUFF_SIZE = 10_000;
+    private static final int CLEAN_UP_THRESHOLD_SIZE = 100_000;
 
-    private List<MessageWithTimestamp> messages = initialList();
+    private final Duration keepAliveDuration;
+    private List<MessageWithTimestamp> messages = new ArrayList<>(INITIAL_BUFF_SIZE);
 
     private NaiveMessageStorage(int messagesKeepTime, TimeUnit unit) {
         keepAliveDuration = Duration.ofNanos(unit.toNanos(messagesKeepTime));
@@ -31,10 +35,17 @@ public class NaiveMessageStorage implements MessageStorage {
 
     @Override
     public void storeMessage(Message message) {
-        MessageWithTimestamp wrappedMsg = MessageWithTimestamp.wrap(message);
+        Instant now = Instant.now();
+
+        MessageWithTimestamp messageWithTimestamp = new MessageWithTimestamp(message, now);
 
         synchronized (this) {
-            messages.add(wrappedMsg);
+
+            if (messages.size() >= CLEAN_UP_THRESHOLD_SIZE) {
+                tryCleanUpSpaceForCurrentMoment(now);
+            }
+
+            messages.add(messageWithTimestamp);
         }
     }
 
@@ -51,24 +62,23 @@ public class NaiveMessageStorage implements MessageStorage {
     }
 
     private Stream<Message> internalQueryLatestStream(int quantity) {
+        Instant now = Instant.now();
 
         if (quantity <= 0) {
             return Stream.empty();
         }
 
-        final Instant lastAcceptableTimestamp = Instant.now().minus(keepAliveDuration);
-
         List<MessageWithTimestamp> result;
 
-        synchronized (this){
+        synchronized (this) {
 
-            if (messages.isEmpty() || lastMessageExceededKeepAliveTime(lastAcceptableTimestamp)) {
-                messages = initialList();
+            tryCleanUpSpaceForCurrentMoment(now);
+
+            if (messages.isEmpty()) {
                 return Stream.empty();
-            }else{
-                dropMessagesWhichExceededKeepAliveTime(lastAcceptableTimestamp);
-                result = internalGetSnapshotOfLast(quantity);
             }
+
+            result = internalSnapshotOfLatestMessages(quantity);
 
         }
 
@@ -76,42 +86,103 @@ public class NaiveMessageStorage implements MessageStorage {
 
     }
 
+    private void tryCleanUpSpaceForCurrentMoment(Instant now) {
+        if (messages.isEmpty()) {
+            return;
+        }
+
+        Instant lastAcceptableTimestamp = now.minus(keepAliveDuration);
+
+        if (lastMessageExceededKeepAliveTime(lastAcceptableTimestamp)) {
+            messages = new ArrayList<>(INITIAL_BUFF_SIZE);
+        } else if (firstMessageExceededKeepAliveTime(lastAcceptableTimestamp)) {
+            dropMessagesWhichExceededKeepAliveTime(lastAcceptableTimestamp);
+        }
+    }
+
     private boolean lastMessageExceededKeepAliveTime(Instant lastAcceptableTimestamp) {
-        return getLast(messages).timestamp.isBefore(lastAcceptableTimestamp);
+        return messages.get(messages.size() - 1).timestamp.isBefore(lastAcceptableTimestamp);
+    }
+
+    private boolean firstMessageExceededKeepAliveTime(Instant lastAcceptableTimestamp) {
+        return messages.get(0).timestamp.isBefore(lastAcceptableTimestamp);
     }
 
     private void dropMessagesWhichExceededKeepAliveTime(Instant lastAcceptableTimestamp) {
 
-        int firstIdxOfKeepAliveMessage = findFirstAcceptableMessageIndex(lastAcceptableTimestamp);
+        int firstIdxOfKeepAliveIdx = findFirstAcceptableMessageIndex(lastAcceptableTimestamp);
 
-        if(firstIdxOfKeepAliveMessage >=0 && firstIdxOfKeepAliveMessage < messages.size()){
-            List<MessageWithTimestamp> messagesWithinKeepAliveTime = messages.subList(firstIdxOfKeepAliveMessage, messages.size());
-            messages = new ArrayList<>(messagesWithinKeepAliveTime.size() * 2);
+        if (firstIdxOfKeepAliveIdx >= 0 && firstIdxOfKeepAliveIdx < messages.size()) {
+            List<MessageWithTimestamp> messagesWithinKeepAliveTime = messages.subList(firstIdxOfKeepAliveIdx, messages.size());
+            messages = new ArrayList<>(Math.max(messagesWithinKeepAliveTime.size() * 2, INITIAL_BUFF_SIZE));
             messages.addAll(messagesWithinKeepAliveTime);
-        }
-        else{
-            messages = initialList();
+        } else {
+            messages = new ArrayList<>(INITIAL_BUFF_SIZE);
         }
 
-    }
-
-    private ArrayList<MessageWithTimestamp> internalGetSnapshotOfLast(int quantity) {
-        int startIdx = Math.max(0, messages.size() - quantity);
-        return new ArrayList<>(messages.subList(startIdx, messages.size()));
     }
 
     private int findFirstAcceptableMessageIndex(Instant lastAcceptable) {
-        int idx = 0;
+        int idx = tryFindNearestIdxToFirstAcceptableMessageUsingBinarySearch(lastAcceptable);
+        return findFirstAcceptableMessageIdxStartingFrom(idx, lastAcceptable);
+    }
+
+    private int tryFindNearestIdxToFirstAcceptableMessageUsingBinarySearch(Instant lastAcceptable) {
+        MessageWithTimestamp searchTarget = MessageWithTimestamp.binarySearchTimestamp(lastAcceptable);
+        List<Comparator<MessageWithTimestamp>> comparators = MessageWithTimestamp.comparatorsOrderedByGranularity();
+
+        int idx = -1;
+        for (Iterator<Comparator<MessageWithTimestamp>> itComparator = comparators.iterator(); idx < 0 && itComparator.hasNext(); ) {
+            idx = binarySearch(messages, searchTarget, itComparator.next());
+        }
+
+        return Math.max(0, idx);
+    }
+
+    private int findFirstAcceptableMessageIdxStartingFrom(int idx, Instant lastAcceptable) {
         while (idx < messages.size() && messages.get(idx).timestamp.isBefore(lastAcceptable)) {
             idx++;
         }
         return idx;
     }
 
-    private static class MessageWithTimestamp{
+    private ArrayList<MessageWithTimestamp> internalSnapshotOfLatestMessages(int quantity) {
+        int startIdx = Math.max(0, messages.size() - quantity);
+        return new ArrayList<>(messages.subList(startIdx, messages.size()));
+    }
+
+    private static class MessageWithTimestamp {
+
+        private static final Comparator<MessageWithTimestamp> hundredMillisGranularityComparator = comparing(
+                MessageWithTimestamp::getTimestamp,
+                comparingLong(Instant::getEpochSecond)
+                        .thenComparingLong(i -> {
+                            long millis = i.toEpochMilli();
+                            return (millis / 100) * 100; // comparison granularity is 100 millis
+                        })
+        );
+
+        private static final Comparator<MessageWithTimestamp> oneSecondGranularityComparator = comparing(
+                MessageWithTimestamp::getTimestamp,
+                comparingLong(Instant::getEpochSecond)
+        );
+
+
+        private static final Comparator<MessageWithTimestamp> oneMinuteGranularityComparator = comparing(
+                MessageWithTimestamp::getTimestamp,
+                comparingLong(i -> i.getEpochSecond() / 60)
+        );
+
+
+        private static final Comparator<MessageWithTimestamp> oneHourGranularityComparator = comparing(
+                MessageWithTimestamp::getTimestamp,
+                comparingLong(i -> i.getEpochSecond() / (60 * 60))
+        );
+
 
         private final Message message;
         private final Instant timestamp;
+
         private MessageWithTimestamp(Message message, Instant timestamp) {
             this.message = message;
             this.timestamp = timestamp;
@@ -119,10 +190,6 @@ public class NaiveMessageStorage implements MessageStorage {
 
         private static MessageWithTimestamp binarySearchTimestamp(Instant timestamp) {
             return new MessageWithTimestamp(null, timestamp);
-        }
-
-        private static MessageWithTimestamp wrap(Message message) {
-            return new MessageWithTimestamp(message, Instant.now());
         }
 
         public Message getMessage() {
@@ -133,8 +200,13 @@ public class NaiveMessageStorage implements MessageStorage {
             return timestamp;
         }
 
-    }
-    private static ArrayList<MessageWithTimestamp> initialList() {
-        return new ArrayList<>(1_000);
+        public static List<Comparator<MessageWithTimestamp>> comparatorsOrderedByGranularity() {
+            return asList(
+                    hundredMillisGranularityComparator,
+                    oneSecondGranularityComparator,
+                    oneMinuteGranularityComparator,
+                    oneHourGranularityComparator
+            );
+        }
     }
 }
